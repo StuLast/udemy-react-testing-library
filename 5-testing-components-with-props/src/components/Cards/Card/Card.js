33323a660221c8ba012ad9fd832f09af9b95ff13
Stu Last <105198429+Stu-Last@users.noreply.github.com>
2022-12-23 11:29:54 +0000

    Implement Card

--- 5-testing-components-with-props/src/components/Cards/Card/Card.js
+++ 5-testing-components-with-props/src/components/Cards/Card/Card.js
@@ -1,10 +1,37 @@
+import { useState } from 'react';
+import heartFilled from '../../../svgs/heartFilled.svg';
+import heartOutlined from '../../../svgs/heartOutlined.svg';
+
+import './Card.css'
+
 const Card = (props) => {
-  const { name, phone, email } = props
+  const { name, phone, email, image, isFavoured } = props;
+  const [isFavouredState, setIsFavouredState] = useState(isFavoured);
+
+  const handleHeartClick = () => {
+    setIsFavouredState(prevState => !prevState);
+  }
+
   return (
-    <div>
-      <h1>{name}</h1>
-      <p>{phone}</p>
-      <p>{email}</p>
+    <div className="card">
+      <div className="card_header">
+        <img alt={image.alt} className="card-img" src={image.url} />
+        <button
+          className="heart"
+          onClick={handleHeartClick}
+        >
+          {
+            isFavouredState ?
+              <img src={heartFilled} alt="filled heart" /> :
+              <img src={heartOutlined} alt="outlined heart" />
+          }
+        </button>
+      </div>
+      <div className="card-content">
+        <h3>{name}</h3>
+        <p>{phone}</p>
+        <p>{email}</p>
+      </div>
     </div>
   )
 };
--- 5-testing-components-with-props/src/components/Cards/Card/__test__/Card.test.js
+++ 5-testing-components-with-props/src/components/Cards/Card/__test__/Card.test.js
@@ -1,4 +1,5 @@
-import { cleanup, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvents from '@testing-library/user-event';
 import Card from '../Card';
 
 const cardProps = {
@@ -14,28 +15,47 @@ const cardProps = {
 
 describe('Testing the card component', () => {
 
-  beforeEach(() => {
-    render(
-      <Card
-        {...cardProps}
-      />);
-  });
-
-  afterEach(() => {
-    cleanup();
-  });
-
   it('should show the name of a cat', () => {
+    render(<Card {...cardProps} />);
     expect(screen.getByRole('heading', { name: cardProps.name })).toBeInTheDocument();
   });
 
   it('should show the phone number', () => {
+    render(<Card {...cardProps} />);
     expect(screen.getByText(cardProps.phone)).toBeInTheDocument();
   });
 
   it('should show the email', () => {
+    render(<Card {...cardProps} />);
     expect(screen.getByText(cardProps.email)).toBeInTheDocument();
   });
 
+  it('should have an image and that src for image is correct', () => {
+    render(<Card {...cardProps} />);
+    expect(screen.getByAltText(cardProps.image.alt).src).toBe(cardProps.image.url);
+  });
 
-})
+  it('should show outlined heart', () => {
+    render(<Card {...cardProps} />);
+    expect(screen.getByAltText('outlined heart')).toBeInTheDocument();
+    expect(screen.queryByAltText('filled heart')).not.toBeInTheDocument();
+  });
+
+  it('should show filled heart', () => {
+    render(<Card {...cardProps} isFavoured={true} />);
+    expect(screen.queryByAltText('outlined heart')).not.toBeInTheDocument();
+    expect(screen.getByAltText('filled heart')).toBeInTheDocument();
+  });
+
+  it('should toggle heart status', () => {
+    render(<Card {...cardProps} />);
+
+    userEvents.click(screen.getByRole('button'));
+    expect(screen.queryByAltText('outlined heart')).not.toBeInTheDocument();
+    expect(screen.getByAltText('filled heart')).toBeInTheDocument();
+
+    userEvents.click(screen.getByRole('button'));
+    expect(screen.getByAltText('outlined heart')).toBeInTheDocument();
+    expect(screen.queryByAltText('filled heart')).not.toBeInTheDocument();
+  });
+});
